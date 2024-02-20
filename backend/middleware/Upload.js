68e5a0e0807c0c35f7441d4346e1042c55e14d50
Sajay0623 <sajay9885@gmail.com>
2024-02-20 08:57:00 +0530

    multer

--- backend/middleware/Upload.js
+++ backend/middleware/Upload.js
@@ -1,16 +1,16 @@
-import React from "react";
-const multer = require("multer");
-import dotenv from "dotenv"
-dotenv.config()
-const { GridFsStorage } = require("multer-gridfs-storage");
-const storage = new GridFsStorage({
-  url: process.env.Mongo_URL,
-});
+// import React from "react";
+// const multer = require("multer");
+// import dotenv from "dotenv"
+// dotenv.config()
+// const { GridFsStorage } = require("multer-gridfs-storage");
+// const storage = new GridFsStorage({
+//   url: process.env.Mongo_URL,
+// });
 
 
-const upload = () => {
+// const upload = () => {
   
-  return <div></div>;
-};
+//   return <div></div>;
+// };
 
-export default upload;
+// export default upload;
--- backend/routes/dashboard.js
+++ backend/routes/dashboard.js
@@ -2,12 +2,12 @@ const express = require("express");
 const RegistrationVal = require("../middleware/RegistrationVal");
 const { Register, Login } = require("../controller/AuthController");
 const AuthorizationMiddleware = require("../middleware/AuthrizationMiddleware");
-const { default: upload } = require("../middleware/Upload");
+// const { default: upload } = require("../middleware/Upload");
 
 const blogRoute = express.Router();
 
 // userRoute.get("/allblog", AuthorizationMiddleware,  );
-blogRoute.post("/file", upload.single("file"));
+// blogRoute.post("/file", upload.single("file"));
 
 module.exports = {
   blogRoute,
